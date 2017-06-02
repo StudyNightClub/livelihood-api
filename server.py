95b5f8c1d8f4c906ebd4f535a0ac16a399352773
Ching Lin <ikafire1230@gmail.com>
2017-06-03 02:11:41 +0800

    [FEATURE] Implement metadata.

--- server.py
+++ server.py
@@ -4,10 +4,22 @@
 from flask import Flask, request, json
 app = Flask(__name__)
 
+class EventsParameters:
+    METADATA = 'metadata'
+
+SCHEMA_FILE = 'response_schema.json'
+with open(SCHEMA_FILE, 'r') as fin:
+    SCHEMA = json.load(fin)
+
 @app.route('/')
 def greet():
     return json.jsonify({'greet': 'Hello there.'})
 
 @app.route('/events')
 def events():
+    metadata = request.args.get(EventsParameters.METADATA, 0, int)
+    if metadata == 1:
+        return json.jsonify(SCHEMA)
+
     return json.jsonify(request.args)
+
